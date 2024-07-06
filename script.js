087d98f480187ce5c20adbd42df1ce5790847c0c
POTHULA. JAI SAI <92910772+jaisaipothula@users.noreply.github.com>
2024-07-06 21:53:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+function findSecondHighest(arr) {
+    let firstHighest = -Infinity;
+    let secondHighest = -Infinity;
+
+    for (let num of arr) {
+        if (num > firstHighest) {
+            secondHighest = firstHighest;
+            firstHighest = num;
+        } else if (num > secondHighest && num !== firstHighest) {
+            secondHighest = num;
+        }
+    }
+
+    return secondHighest;
+}
